7986b7e791f1885b6930b9814c2fac8f7d49f544
Jonas Wagner <jonas@29a.ch>
2012-07-29 15:45:36 +0200

    Added lots of stuff.

--- src/main.js
+++ src/main.js
@@ -2,10 +2,26 @@
 
 requires('jquery');
 requires('gl-matrix');
+requires('game-shim');
 requires('webgl-debug');
 
 provides('main');
 
-var c = $('canvas');
+var Loader = requires('loader').Loader,
+    Clock = requires('clock').Clock;
+
+var RESOURCES = [
+    'shaders/voxel.vertex',
+    'shaders/voxel.frag'
+];
+
+var loader = new Loader(),
+    resources = loader.resources,
+    c = $('canvas')[0];
+
+loader.onready = function() {
+};
+
+loader.load(RESOURCES);
 
 })();
